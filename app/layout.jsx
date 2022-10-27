05c06ea42826d6278ea21a1ddf1fcec0f72aa71b
theItalianDev <italiandev@protonmail.com>
2022-10-27 22:49:12 +0200

    meta viewport added

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,7 @@ export default async function RootLayout({children}) {
       <head>
         <title>Playing with Next13</title>
         <meta name="description" content="Playing with Next13" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className='font-mono flex flex-col'>
