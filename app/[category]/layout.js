f481f0a99b8c590a93292baec780bcd215ee06c3
theItalianDev <italiandev@protonmail.com>
2022-10-28 18:33:53 +0200

    back button added

--- app/[category]/layout.js
+++ app/[category]/layout.js
@@ -3,7 +3,7 @@ import Breadcumb from "./Breadcumb";
 export default function CategoryLayout({children}) {
     return(
         <div className="p-20 w-full">
-            <Breadcumb>{`\<- back`}</Breadcumb>
+            <Breadcumb>{`\<-`}</Breadcumb>
             {children}
         </div>
     )
